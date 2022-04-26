0c174be1b0689f0d22c09561006b057b2588ee43
PyrEnd <endertev@gmail.com>
2022-04-26 05:01:48 -0400

    Primer commit

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,49 +1,59 @@
 const { response, request } = require('express');
 
-const usuariosGet = (req = require, res = response)=>{
+const usuariosGet = (req = request, res = response) => {
 
-    const query = req.query;
+    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
 
-    res.status(200).json({
-        message: 'get API - controlador'
-    })
-}
-
-const usuariosPut = (req, res = response)=>{
-
-    const id = req.params.id;
-
-    res.status(201).json({
-        message: 'put API - controlador',
-        id
-    })
-}
-
-const usuariosPost = (req, res = response)=>{
-    const { nombre, edad } = req.body;
-    
-    res.status(201).json({
-        message: 'post API - controlador',
-        nombre
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page, 
+        limit
     });
 }
 
-const usuariosDelete = (req, res = response)=>{
-    res.status(201).json({
-        message: 'delete API - controlador'
-    })
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - usuariosPost',
+        nombre, 
+        edad
+    });
 }
 
-const usuariosPatch = (req, res = response)=>{
-    res.status(201).json({
-        message: 'patch API - controlador'
-    })
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - usuariosPut',
+        id
+    });
 }
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });
+}
+
+
+
 
 module.exports = {
     usuariosGet,
-    usuariosPut,
     usuariosPost,
+    usuariosPut,
+    usuariosPatch,
     usuariosDelete,
-    usuariosPatch
 }
